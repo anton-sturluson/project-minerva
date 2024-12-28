d9077d4b41337be4f60374fe0175df88ff10f120
Anton Sturluson <anton.sturluson@gmail.com>
2024-12-28 18:03:40 -0500

    feat: improve codebase to include Anthropic

--- src/minerva/llm/client.py
+++ src/minerva/llm/client.py
@@ -1,9 +1,32 @@
+from abc import ABC, abstractmethod
+
+from anthropic import Anthropic
 from openai import OpenAI
 
 
-class Client:
+class Client(ABC):
     """Base class for LLM clients."""
-    pass
+    @abstractmethod
+    def get_completion(
+        self,
+        prompt: str,
+        model: str = "gpt-4o-mini",
+        temperature: float = 0.1,
+        max_tokens: int = 4096,
+    ) -> str:
+        """
+        Abstract method for getting a completion from an LLM.
+        
+        Args:
+            prompt (str): The input prompt for the LLM.
+            model (str, optional): The model to use. Defaults to "gpt-4o-mini".
+            temperature (float, optional): Sampling temperature. Defaults to 0.1.
+            max_tokens (int, optional): Maximum number of tokens to generate. Defaults to 4096.
+        
+        Returns:
+            str: The generated completion.
+        """
+        raise NotImplementedError
 
 
 class OpenAIClient(Client):
@@ -35,3 +58,24 @@ class OpenAIClient(Client):
             max_tokens=max_tokens,
         )
         return out.choices[0].message.content
+
+
+class AnthropicClient(Client):
+    """Anthropic client."""
+    def __init__(self, api_key: str):
+        self.client = Anthropic(api_key=api_key)
+
+    def get_completion(
+        self,
+        prompt: str,
+        model: str = "claude-3-5-sonnet-latest",
+        temperature: float = 0.1,
+        max_tokens: int = 4096,
+    ) -> str:
+        out = self.client.messages.create(
+            model=model,
+            messages=[{"role": "user", "content": prompt}],
+            temperature=temperature,
+            max_tokens=max_tokens,
+        )
+        return out.content[0].text
