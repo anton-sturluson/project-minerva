8f137899e434764870c943bd22c301c2584f06db
Anton Sturluson <8998150+anton-sturluson@users.noreply.github.com>
2025-01-12 21:46:51 -0500

    Feature/issue 7 (#10)
    * feat: adapt `init_milvus` to run with recent changes in chunking output
    
    * feat: change output fields for query
    
    * feat: change format of text embedding in vector db
    
    * feat: remove single or double quotes in `llm.useful`
    
    * feat: make sure the output prompts aren't too long in `llm.useful`
    
    * style: linter
    
    * feat: add a prompt not to give explanation
    
    * feat: sort vector DB query output in a descending order
    
    * feat: change text format for embedding

--- src/init_milvus.py
+++ src/init_milvus.py
@@ -23,8 +23,8 @@ DEFAULT_DB_PATH: str = "../data/milvus_demo.db"
 DEFAULT_COLLECTION_NAME: str = "demo_collection"
 
 
-def init_milvus(
-    db_path: str = DEFAULT_DB_PATH, 
+def _init(
+    db_path: str = DEFAULT_DB_PATH,
     collection_name: str = DEFAULT_COLLECTION_NAME
 ) -> MilvusVectorDB:
     """Initialize Milvus Vector Database."""
@@ -43,7 +43,7 @@ def init_milvus(
     schema.add_field(field_name="speaker", datatype=DataType.VARCHAR, max_length=50)
     schema.add_field(field_name="year", datatype=DataType.INT64)
     schema.add_field(field_name="quarter", datatype=DataType.INT64)
-    schema.add_field(field_name="transcript_index", datatype=DataType.INT64)
+    schema.add_field(field_name="speaker_index", datatype=DataType.INT64)
     schema.add_field(field_name="chunk_index", datatype=DataType.INT64)
     schema.add_field(field_name="vector", datatype=DataType.FLOAT_VECTOR, dim=1536)
 
@@ -60,42 +60,62 @@ def init_milvus(
     return db
 
 
+def _get_text_to_embed(
+    ticker: str,
+    company: str, 
+    speaker: str,
+    fiscal_year: int,
+    quarter: int,
+    topic: str, 
+    text: str
+) -> str:
+    """
+    Transform transcript into a format to embed into Milvus.
+    """
+    company_info: str = f"company: {company} (ticker: {ticker})"
+    fiscal_info: str = f"FY{fiscal_year} Q{quarter}"
+    speaker_info: str = f"speaker: {speaker}, topic: {topic}"
+    return f"[{company_info}, {fiscal_info}, {speaker_info}]\n{text}"
+
+
 def add_documents(
-    db: MilvusVectorDB, 
+    db: MilvusVectorDB,
     kb: CompanyKB,
     embedding_fn: Callable,
     ticker: str
 ):
     """Add documents in the KB to Milvus Vector Database."""
-    for company_map in tqdm(kb.transcripts.find({"ticker": ticker}), desc="Adding documents"):
+    query = {"ticker": ticker}
+    for company_map in tqdm(kb.transcripts.find(query), desc="Adding documents"):
+        company_name: str = company_map["company_name"]
         for transcript_map in tqdm(company_map["transcripts"], desc="Adding transcripts"):
             year: int = transcript_map["year"]
             quarter: int = transcript_map["quarter"]
-            company_name: str = company_map["company_name"]
 
-            transcript_index_map: dict[int, dict] = {
-                line_map["index"]: line_map
-                for line_map in transcript_map["transcript"]
-            }
             docs: list[str] = []
             metadata: list[dict] = []
-            for chunk_map in transcript_map["chunks"]:
-                transcript_index: int = chunk_map["transcript_index"]
-                transcript_line_map: dict = transcript_index_map.get(transcript_index)
-                if not transcript_line_map:
-                    continue
+            for chunk_map in transcript_map.get("chunking_output", []):
+                speaker: str = chunk_map.get("speaker", "")
 
-                docs.append(chunk_map["chunk"])
-                metadata.append({
-                    "text": chunk_map["chunk"],
-                    "ticker": ticker,
-                    "company_name": company_name,
-                    "speaker": transcript_line_map["speaker"],
-                    "year": year,
-                    "quarter": quarter,
-                    "transcript_index": transcript_index,
-                    "chunk_index": chunk_map["chunk_index"]
-                })
+                for chunk in chunk_map.get("chunks", []):
+                    text: str = chunk.get("text")
+                    topic: str = chunk.get("chunk_topic", "")
+                    if not text:
+                        continue
+
+                    embed_text: str = _get_text_to_embed(
+                        ticker, company_name, speaker, year, quarter, topic, text)
+                    docs.append(embed_text)
+                    metadata.append({
+                        "text": embed_text,
+                        "ticker": ticker,
+                        "company_name": company_name,
+                        "speaker": speaker,
+                        "year": year,
+                        "quarter": quarter,
+                        "speaker_index": chunk_map["speaker_index"],
+                        "chunk_index": chunk["chunk_index"]
+                    })
 
             db.add_documents(
                 documents=docs,
@@ -106,27 +126,27 @@ def add_documents(
 
 @click.command()
 @click.option(
-    "--query", type=str, default="", 
+    "--force-init", type=bool, is_flag=True,
+    help="Whether to force initializing the Milvus Vector Database")
+@click.option(
+    "--query", type=str, default="",
     help="Query to search for in the vector database")
 @click.option(
     "--tickers", type=str, default="all",
     help="Ticker of the companies (separated by comma) to include")
 @click.option(
-    "--db-path", type=str, default="../data/milvus_demo.db", 
+    "--db-path", type=str, default="../data/milvus_demo.db",
     help=("Path to the Milvus Vector Database. If not provided, a new database "
           "will be created.  "))
 @click.option(
-    "--query-save-dir", type=str, default="../data/query_results", 
+    "--query-save-dir", type=str, default="../data/query_results",
     help="Directory to save query results")
-def main(query: str, tickers: str, db_path: str, query_save_dir: str):
-    if not query:
-        raise ValueError("Query is required")
-
+def main(force_init: bool, query: str, tickers: str, db_path: str, query_save_dir: str):
     kb = CompanyKB()
     openai_client = OpenAIClient(api_key=OPENAI_API_KEY)
     embedding_fn: Callable = openai_client.get_embedding
 
-    if File(db_path).exists:
+    if not force_init and File(db_path).exists:
         logger.info("Loading Milvus Vector DB from %s...", db_path)
         db: MilvusVectorDB = MilvusVectorDB(
             db_path=db_path, collection_name=DEFAULT_COLLECTION_NAME)
@@ -134,25 +154,31 @@ def main(query: str, tickers: str, db_path: str, query_save_dir: str):
         if not db_path:
             db_path = DEFAULT_DB_PATH
         logger.info("Initializing Milvus Vector Database...")
-        db: MilvusVectorDB = init_milvus(db_path=db_path)
+        db: MilvusVectorDB = _init(db_path=db_path)
         if tickers == "all":
-            tickers = kb.tickers
-        for ticker in tickers.split(","):
-            add_documents(db, embedding_fn, ticker.strip())
+            tickers = kb.unique_tickers
+        else:
+            tickers = tickers.split(",")
 
-    output_fields: list[str] = ["text", "speaker", "year", "quarter", 
-                                "transcript_index", "chunk_index"]
-    res: list[list[dict]] = db.search(
-        queries=query,
-        embedding_fn=embedding_fn,
-        # filter=f"ticker == '{ticker}'",
-        output_fields=output_fields,
-        limit=20,
-    )
-    file_name: str = llm.generate_filename(openai_client, query, ext=".yml")
-    file_path: File = File(query_save_dir) / file_name
-    logging.info("Saving query results to %s...", file_path)
-    file_path.save(res[0])
+        for ticker in tickers:
+            add_documents(db, kb, embedding_fn, ticker)
+
+    if query:
+        output_fields: list[str] = [
+            "company_name", "year", "quarter", "text",
+            "speaker", "speaker_index", "chunk_index", "text"]
+        res: list[list[dict]] = db.search(
+            queries=query,
+            embedding_fn=embedding_fn,
+            # filter=f"ticker == '{ticker}'",
+            output_fields=output_fields,
+            limit=50,
+        )
+        res = res[0][::-1] # FIXME
+        file_name: str = llm.generate_filename(query, ext=".yml")
+        file_path: File = File(query_save_dir) / file_name
+        logging.info("Saving query results to %s...", file_path)
+        file_path.save(res)
 
 
 if __name__ == "__main__":
--- src/minerva/llm/useful.py
+++ src/minerva/llm/useful.py
@@ -10,8 +10,13 @@ def _init_client(model_name: str) -> Client:
         return OpenAIClient(OPENAI_API_KEY)
 
 
+def _preprocess(text: str) -> str:
+    """Strip single or double quotes from the text."""
+    return text.replace('"', "").replace("'", "")
+
+
 def generate_filename(
-    text: str, 
+    text: str,
     model_name: str = "claude-3-5-haiku-latest",
     temperature: float = 0.3,
     max_tokens: int = 4096,
@@ -36,10 +41,11 @@ def generate_filename(
     """
     prompt: str = f"""
     Given the following text, generate a concise and clear file name that summarizes the key
-    content in a format appropriate for saving as a file. The file name should be brief, avoid
-    special characters (except hyphens or underscores), and reflect the primary topic or
-    purpose of the text. It should be clear and recognizable, such that someone can infer the
-    content of the file without opening it.
+    content in a format appropriate for saving as a file. The file name should be brief, **no
+    more than 7 words**, avoid special characters (except hyphens or underscores),
+    and reflect the primary topic or purpose of the text. It should be clear and recognizable,
+    such that someone can infer the content of the file without opening it.
+    **DO NOT** provide explanation.
 
     <Example>
     Example Input:
@@ -54,9 +60,9 @@ def generate_filename(
     "{text}"
     """
     client: Client = _init_client(model_name)
-    name: str = client.get_completion(prompt, model=model_name, 
+    name: str = client.get_completion(prompt, model=model_name,
                                       temperature=temperature, max_tokens=max_tokens)
-    return name + ext
+    return _preprocess(name + ext)
 
 
 def try_test_prompt(client: Client) -> str:
@@ -65,7 +71,7 @@ def try_test_prompt(client: Client) -> str:
 
 
 def generate_topic(
-    text: str, 
+    text: str,
     model_name: str = "claude-3-5-haiku-latest",
     temperature: float = 0.3,
     max_tokens: int = 4096,
@@ -87,9 +93,10 @@ def generate_topic(
             depends on the LLM client.
     """
     prompt: str = f"""
-        Extract the single most important topic from the text below, using 1-5 words in a clear, concise phrase. 
-        If the text lacks any substantive topic or meaningful content, output exactly 'Generic Topic':
-        
+        Extract the single most important topic from the text below, using **1-5 words**
+        in a clear, concise phrase. If the text lacks any substantive topic or meaningful content,
+        output exactly 'Generic Topic':
+
         <Text>
         {text}
         </Text>
@@ -97,11 +104,12 @@ def generate_topic(
         <Example>
         Example with topic: "The rapid advancement of artificial intelligence has led to breakthroughs in healthcare diagnostics."
         Output: AI healthcare diagnostics
-        
+
         Example without topic: "Things happened and stuff occurred in places with people."
         Output: Generic Topic
         </Example>
     """
     client: Client = _init_client(model_name)
-    return client.get_completion(prompt, model=model_name, 
-                                 temperature=temperature, max_tokens=max_tokens)
+    output: str = client.get_completion(prompt, model=model_name,
+                                        temperature=temperature, max_tokens=max_tokens)
+    return _preprocess(output)
