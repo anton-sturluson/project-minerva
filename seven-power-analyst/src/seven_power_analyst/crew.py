31bc40321e5535d75bb3696a08d0941647a5554c
Anton Sturluson <anton.sturluson@gmail.com>
2025-02-06 22:33:21 -0500

    feat: tmp push

--- seven-power-analyst/src/seven_power_analyst/crew.py
+++ seven-power-analyst/src/seven_power_analyst/crew.py
@@ -1,9 +1,11 @@
-from crewai import Agent, Crew, Process, Task
+from crewai import Agent, Crew, Process, Task, LLM
 from crewai.project import CrewBase, agent, crew, task
 
 # If you want to run a snippet of code before or after the crew starts, 
 # you can use the @before_kickoff and @after_kickoff decorators
 # https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
+crew_llm = LLM(model="gpt-4o-mini", temperature=0.5)
+manager_llm = LLM(model="gpt-4o", temperature=0.5)
 
 @CrewBase
 class SevenPowerAnalyst():
@@ -21,6 +23,7 @@ class SevenPowerAnalyst():
 	def company_analyst(self) -> Agent:
 		return Agent(
 			config=self.agents_config['company_analyst'],
+			llm=crew_llm,
 			verbose=True
 		)
 
@@ -28,34 +31,56 @@ class SevenPowerAnalyst():
 	def company_analyst_critic(self) -> Agent:
 		return Agent(
 			config=self.agents_config['company_analyst_critic'],
+			llm=crew_llm,
+			verbose=True
+		)
+
+	@agent
+	def hamilton_helmer(self) -> Agent:
+		return Agent(
+			config=self.agents_config['hamilton_helmer'],
+			llm=manager_llm,
+			verbose=True
+		)
+
+	@agent
+	def sector_analyst(self) -> Agent:
+		return Agent(
+			config=self.agents_config['sector_analyst'],
+			llm=crew_llm,
 			verbose=True
 		)
 
 	# To learn more about structured task outputs, 
 	# task dependencies, and task callbacks, check out the documentation:
 	# https://docs.crewai.com/concepts/tasks#overview-of-a-task
-	@task
-	def initial_company_analysis_task(self) -> Task:
-		return Task(
-			config=self.tasks_config['company_analysis_task'],
-			description="Conduct initial comprehensive analysis of the company following the provided framework.",
-			output_file='output/initial_report.md'
-		)
+	# @task
+	# def initial_company_analysis_task(self) -> Task:
+	# 	return Task(
+	# 		config=self.tasks_config['company_analysis_task'],
+	# 		output_file='output/initial_report.md'
+	# 	)
 
-	@task
-	def company_analysis_critique_task(self) -> Task:
-		return Task(
-			config=self.tasks_config['company_analysis_critique_task'],
-			input_files=['output/initial_report.md'],
-			output_file='output/critique.md'
-		)
+	# @task
+	# def company_analysis_critique_task(self) -> Task:
+	# 	return Task(
+	# 		config=self.tasks_config['company_analysis_critique_task'],
+	# 		input_files=['output/initial_report.md'],
+	# 		output_file='output/critique.md'
+	# 	)
+
+	# @task
+	# def sector_analysis_task(self) -> Task:
+	# 	return Task(
+	# 		config=self.tasks_config['sector_analysis_task'],
+	# 		output_file='output/sector_analysis.md'
+	# 	)
 
 	@task
 	def final_company_analysis_task(self) -> Task:
 		return Task(
-			config=self.tasks_config['company_analysis_task'],
-			description="Revise and enhance the initial analysis based on the critique provided.",
-			input_files=['output/initial_report.md', 'output/critique.md'],
+			config=self.tasks_config['seven_power_analysis_task'],
+			# input_files=['output/initial_report.md', 'output/critique.md', 'output/sector_analysis.md'],
 			output_file='output/final_report.md'
 		)
 
@@ -66,9 +91,8 @@ class SevenPowerAnalyst():
 		# https://docs.crewai.com/concepts/knowledge#what-is-knowledge
 
 		return Crew(
-			agents=self.agents, # Automatically created by the @agent decorator
-			tasks=self.tasks, # Automatically created by the @task decorator
+			agents=self.agents,
+			tasks=self.tasks,
 			process=Process.sequential,
 			verbose=True,
-			# process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
 		)
