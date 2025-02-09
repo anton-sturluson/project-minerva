24e35d5b80f75331c067d9ac2e4d9d2aa5cf2ba8
Anton Sturluson <anton.sturluson@gmail.com>
2025-02-08 20:30:21 -0500

    build: fix pyproject to run everything

---
+++ knowledge-base/src/knowledge/__init__.py
@@ -0,0 +1,4 @@
+"""Minerva Knowledge Base package."""
+from knowledge.__about__ import __version__
+
+__all__ = ["__version__"]
---
+++ knowledge-base/src/knowledge/database/__init__.py
@@ -0,0 +1,4 @@
+"""Database module for Minerva Knowledge Base."""
+from knowledge.database.kb import CompanyKB
+
+__all__ = ["CompanyKB"] 
